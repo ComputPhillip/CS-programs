792202f8af7377894980d2a0157a5c7c502d0a69
ComputPhillip <106040625+ComputPhillip@users.noreply.github.com>
2023-01-20 00:08:03 +0000

    init

---
+++ classJan19/source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Johnson   Marieta     85   83  77   91  76
+// Aniston     Saboly      80   90   95   93  48
+// Cooper     Mancini    81   91   90   83  58
+// Gupta       Piota        70   50   85   73  68
+// Blair          Coviani    60   90   75   53  98
+
+int main(){
+    int grades[5][5] = {{ 85, 83, 77, 91, 76 },
+                       { 80, 90, 95, 93, 48 },
+                       { 81, 91, 90, 83, 58 }, 
+                       { 70, 50, 85, 73, 68 },
+                       { 60, 90, 75, 53, 98 }};
+
+    std::fstream outfile;
+    outfie.open();
+
+    for (int i = 0; i < 5; i++){
+    for (int j = 0; j < 5; j++){
+        //std::cout << grades[i][j] << ' ';
+    }
+        //std::cout << std::endl;
+    }
+
+
+}
