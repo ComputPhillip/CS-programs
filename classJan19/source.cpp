48a3423604bd8dd73ddae62244306f0731893c5f
ComputPhillip <106040625+ComputPhillip@users.noreply.github.com>
2023-01-20 00:29:45 +0000

    1 hrs work

--- classJan19/source.cpp
+++ classJan19/source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 // Johnson   Marieta     85   83  77   91  76
 // Aniston     Saboly      80   90   95   93  48
@@ -14,15 +15,24 @@ int main(){
                        { 81, 91, 90, 83, 58 }, 
                        { 70, 50, 85, 73, 68 },
                        { 60, 90, 75, 53, 98 }};
+    std::string names [5][2] = {{"Johnson"}, {"Marieta"},
+                                {"Aniston"}, {"Saboly"},
+                                {"Cooper"}, {"Mancini"},
+                                {"Gupta"}, {"Piota"},
+                                {"Blair"}, {"Coviani"},};
 
-    std::fstream outfile;
-    outfie.open();
+    std::fstream outFile;
+    outFile.open("grades.txt", std::ios::out);
+    if(outFile.is_open()){
+    std::cout << "file is open\n";
+    }else
+    std::cout << "file is closed, retry attempt\n";
 
     for (int i = 0; i < 5; i++){
-    for (int j = 0; j < 5; j++){
-        //std::cout << grades[i][j] << ' ';
-    }
-        //std::cout << std::endl;
+        for (int j = 0; j < 5; j++){
+            outFile << grades[i][j] << ' ';
+        }
+    outFile << '\n';
     }
 
 
