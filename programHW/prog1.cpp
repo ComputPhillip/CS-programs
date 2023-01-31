b3c2b2bd8db0de13ea153c775fe0e764340d30d5
ComputPhillip <106040625+ComputPhillip@users.noreply.github.com>
2023-01-30 23:07:45 -0600

    Update Prog1.cpp

--- programHW/prog1.cpp
+++ programHW/prog1.cpp
@@ -1,28 +1,42 @@
-#include <iostream>
+//author: Phillip Gooden
+//submitted: 01-30-2023
+//using three array(string names, int grades, and average)
+//calculate the sudents average test scores and display it
 #include <fstream>
 #include <string>
-#include <sstream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    string line;
-    fstream file("data.txt", ios::in);
-    while (!file.eof()){
-        getline(file, line);
-        stringstream ss(line);
-        string firstName;
-        getline(ss, firstName, ' ');
-        string lastName;
-        getline(ss, lastName, ' ');
-        //cout << firstName << ' ' << lastName << endl;
-        int grades[5];
-        for(int i {0}; i < 5; i++){
-            ss >> grades[i];
-            cout << grades[i] << ' ';
-        }      
+const int GRADES = 5;
+
+void Read(int scores[GRADES], string firstName, string lastName, double avg);
+
+int main() {
+    string firstName, lastName;
+    int scores[GRADES];
+    double avg = 0.0;
+    Read(scores,firstName,lastName, avg);
+}
+
+void Read(int scores[GRADES], string firstName, string lastName, double avg){
+    ifstream file("data.txt");
+    double total = 0;
+    while (file >> lastName >> firstName) {
+        double total = 0;
+        for (int i = 0; i < GRADES; i++) {
+            file >> scores[i];
+            total += scores[i];
+        }
+        avg = total / GRADES;
+        cout << "First Name: " << firstName << endl;
+        cout << "Last Name: " << lastName << endl;
+        cout << "Scores: ";
+        for (int i = 0; i < GRADES; i++) {
+            cout << scores[i] << " ";
+        }
         cout << endl;
+        cout << "Average: " << avg << endl << endl;
     }
     file.close();
 }
-
