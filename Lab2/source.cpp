f3b92cba8bde01fbc3084e90a39aca13611f0578
ComputPhillip <106040625+ComputPhillip@users.noreply.github.com>
2023-02-15 19:15:11 -0600

    Lab2 Complete

--- Lab2/source.cpp
+++ Lab2/source.cpp
@@ -5,6 +5,8 @@
 //Finally display the information
 #include<iostream>
 #include<string>
+#include<iomanip>
+
 
 using namespace std;
 
@@ -13,10 +15,25 @@ struct MovieData{
     string director;//director of movie
     int year;//year movie was created
     int length;//length of movie
+    long double revenue; //total revenue for movie
 };
 
 //void display(MovieData m1)
+void Display(MovieData m);
 
 int main(){
-    MovieData m1 = {"Devotion", }
+    MovieData m1 = { "Avatar: The Way of Water", "James Cameron", 2022, 192, 2'214'990'702 };
+    MovieData m2 = { "Black Panther: Wakanda Forever", "Ryan Coogler", 2022, 162, 855'000'000 };
+    Display(m1);
+    Display(m2);
+    return 0;
+}
+
+void Display(MovieData m){
+    cout << left  << setw(20) << "Movie Title: " << m.title << '\n' 
+    << left  << setw(20) << "Movie Director: "   << m.director << '\n'
+    << left  << setw(20) << "Movie Length: "     << m.length << '\n' 
+    << left  << setw(20) << "Movie Year: "       << m.year << '\n'
+    << left  << setw(20) << "Movie Revenue: ";
+    std::cout << '$' << std::put_money(m.revenue) << std::endl << std::endl;
 }
